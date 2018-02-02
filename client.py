5ade10af5b1a15d9433ffcc9e4ff4e3d85d4788a
Li Haomin <lhmlihaomin@live.com>
2018-02-02 18:03:39 +0800

    some apis

--- client.py
+++ client.py
@@ -1,10 +1,18 @@
-import os
+import os, json
 import ConfigParser
 
 from QcloudApi.qcloudapi import QcloudApi
 
+API_VERSION = '2017-03-12'
+
 class QcloudClient(object):
     """Qcloud Client with handy features."""
+    def __init__(self, profile_name='default'):
+        self.profile_name = profile_name
+        self.parse_config()
+        self.parse_credentials()
+        
+    ##### Configuration methods #####
     @property
     def default_config_file(self):
         return os.path.sep.join([
@@ -52,16 +60,73 @@ class QcloudClient(object):
         self.secret_id = secret_id
         self.secret_key = secret_key
 
-    def __init__(self, profile_name='default'):
-        self.profile_name = profile_name
-        self.parse_config()
-        self.parse_credentials()
-        
-    def service(module):
+    ##### Result parsing #####
+    def parse_result(self, action, result):
+        result_keys = {
+            'DescribeInstances': 'InstanceSet'
+        }
+        result = json.loads(result)
+        result = result['Response']
+        if result.has_key('Error'):
+            return (False, result)
+        if result_keys.has_key(action):
+            return (True, result[result_keys[action]])
+        else:
+            return (True, result)
+
+    ##### Service methods #####
+    def set_service(self, module):
+        """Set service object with module"""
         config = {
             'Region': self.region,
             'secretId': self.secret_id,
             'secretKey': self.secret_key,
             'method': 'post'
         }
-        QcloudApi(module, config)
+        self.service = QcloudApi(module, config)
+
+    def describe_instances(self):
+        """List CVM instance information"""
+        self.set_service('cvm')
+        action = 'DescribeInstances'
+        params = {'Version': '2017-03-12'}
+        result = self.service.call(action, params)
+        result = self.parse_result(action, result)
+        return result
+
+    def run_instances(self):
+        pass
+
+    def start_instances(self, instance_ids):
+        """Start (boot) CVM instances"""
+        self.set_service('cvm')
+        action = 'StartInstances'
+        params = {
+            'Version': API_VERSION,
+            'InstanceIds': instance_ids,
+        }
+        result = self.service.call(action, params)
+        result = self.parse_result(action, result)
+        return result
+
+    def stop_instances(self, instance_ids):
+        """Stop (shutdown) CVM instances"""
+        self.set_service('cvm')
+        action = 'StopInstances'
+        params = {
+            'Version': API_VERSION,
+            'InstanceIds': instance_ids,
+        }
+        result = self.service.call(action, params)
+        result = self.parse_result(action, result)
+        return result
+
+    def modify_instance_security_group(self):
+        pass
+
+    def modify_instance_tags(self):
+        pass
+
+    def modify_volume_tags(self):
+        pass
+
--- run.py
+++ run.py
@@ -1,4 +1,11 @@
+import sys
 from client import QcloudClient
 
+instance_ids = sys.argv[1:]
+
 client = QcloudClient()
+#result = client.describe_instances()
+#result = client.stop_instances(instance_ids)
+result = client.start_instances(instance_ids)
+print result
 
