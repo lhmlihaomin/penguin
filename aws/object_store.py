704b76b259d121e73527156b26529e5f37be16ae
Li Haomin <lhmlihaomin@live.com>
2018-06-01 17:54:50 +0800

    * fixed README toc * added S3 support

---
+++ aws/object_store.py
@@ -0,0 +1,22 @@
+import os
+import boto3
+
+class Client(object):
+    """Pseudo client for AWS S3 service"""
+    def __init__(self, profile_name, region_name):
+        self.profile_name = profile_name
+        self.region_name = region_name
+
+        self.s3 = boto3.Session(
+            profile_name=self.profile_name,
+            region_name=self.region_name
+            ).resource('s3')
+
+    def download_file(self, bucket, key, local_path):
+        bucket = self.s3.Bucket(bucket)
+        bucket.download_file(key, local_path)
+        
+
+    def upload_file(self, bucket, key, local_path):
+        bucket = self.s3.Bucket(bucket)
+        bucket.download_file(local_path, key)
